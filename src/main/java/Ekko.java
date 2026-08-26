dc4ac6e7df81c16e646fe500745cdfa4e0bbb857
IzN432 <isaac.ng432@gmail.com>
2026-08-26 13:20:38 +0800

    Rename Duke chatbot to Ekko
    The application and documentation identify the chatbot as Duke.
    
    Use Ekko consistently so the entry point, startup banner, setup guide,
    and user guide agree on the product name.

---
+++ src/main/java/Ekko.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Ekko chatbot application.
+ */
+public class Ekko {
+    public static void main(String[] args) {
+        String banner = " _____ _    _          \n"
+                + "| ____| | _| | _____   \n"
+                + "|  _| | |/ / |/ / _ \\\n"
+                + "| |___|   <|   < (_) | \n"
+                + "|_____|_|\\_\\_|\\_\\___/  \n";
+        System.out.println(banner);
+    }
+}
